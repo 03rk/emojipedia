98d1f0cb48feca3f64f0f5577c9fbe49456bd473
Rahul kumar <100887190+03rk@users.noreply.github.com>
2022-04-13 09:28:45 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p> Created with ❤ ️Rahul</p>
+      <hr></hr>
+      <p>Copyright© 2022</p>
+    </footer>
+  );
+}
+export default Footer;
